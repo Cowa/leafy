0c674df3ae057cb7e535e9909dfc3efdf1edef7c
Brice Thomas <brice.p.thomas@gmail.com>
2015-10-02 09:50:56 +0200

    Add select[Type] to bucket

--- src/main/scala/leafy/models/Bucket.scala
+++ src/main/scala/leafy/models/Bucket.scala
@@ -5,6 +5,9 @@ object Bucket {
 }
 
 case class Bucket(source: String, data: List[Data]) {
+  import scala.reflect.ClassTag
+
   def add(d: Data) = Bucket(source, data :+ d)
   def add(d: List[Data]) = Bucket(source, data ++ d)
+  def select[T: ClassTag] = data.collect({case t: T => t})
 }
